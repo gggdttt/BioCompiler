00bf0621162f1a1ae5f0a4daaf4ee47dcba8de0e
gggdttt <wenjiedtu@gmail.com>
2022-11-14 15:26:45 +0100

    add more test in executor

--- TestExecutor/TestExecutor.cs
+++ TestExecutor/TestExecutor.cs
@@ -19,6 +19,23 @@ namespace TestExecutor
 
 
         [TestMethod]
+        public void TestMove()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "droplet d2;\r\n\r\n" +
+                "input(d1,1,1,1.0);\r\n" +
+                "input(d2,4,4,0.5);\r\n\r\n\r\n" +
+                "move(d1,3,3);\r\n" +
+                "move(d2,7,7);\r\n\r\n" +
+                "output(d1,0,0);\r\n" +
+                "output(d2,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+        [TestMethod]
         public void TestMultiMove()
         {
             string origin =
@@ -73,6 +90,133 @@ namespace TestExecutor
             Assert.AreEqual(true, c.manager.AllTasksCompleted());
         }
 
+        [TestMethod]
+        public void TestMerger()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "droplet d2;\r\n" +
+                "droplet d3;\r\n" +
+                "input(d1,1,1,1.0);\r\n" +
+                "input(d2,4,4,0.5);\r\n" +
+                "merge(d3,d1,d2,5,9);\r\n" +
+                "output(d3,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+        [TestMethod]
+        public void TestMultiMerger()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "droplet d2;\r\n" +
+                "droplet d3;\r\n" +
+                "input(d1,1,1,1.0);\r\n" +
+                "input(d2,4,4,0.5);\r\n" +
+                "input(d3,10,10,3.2);\r\n\r\n" +
+                "droplet d4;\r\n" +
+                "droplet d5;\r\n" +
+                "merge(d4,d1,d2,5,9);\r\n\r\n" +
+                "merge(d5,d4,d3,5,9);\r\n\r\n" +
+                "output(d5,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+        [TestMethod]
+        public void TestMixer()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "input(d1,1,1,1.0);\r\n" +
+                "mix(d1,2,2,2,2,5);\r\n\r\n" +
+                "output(d1,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+        [TestMethod]
+        public void TestMultiMixer()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "input(d1,1,1,1.0);\r\n" +
+                "mix(d1,2,2,2,2,5);\r\n\r\n" +
+                "mix(d1,2,2,2,2,5);\r\n\r\n" +
+                "mix(d1,2,2,2,2,5);\r\n\r\n" +
+                "output(d1,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+
+        [TestMethod]
+        public void TestSplitter()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "droplet d2;\r\n" +
+                "droplet d3;\r\n\r\n" +
+                "input(d1,1,1,2.0);\r\n" +
+                "split(d2,d3,d1,12,12,15,15,0.5);\r\n\r\n" +
+                "output(d2,0,0);\r\n" +
+                "output(d3,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+        [TestMethod]
+        public void TestMultiSplitter()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "droplet d2;\r\n" +
+                "droplet d3;\r\n\r\n" +
+                "input(d1,1,1,2.0);\r\n" +
+                "split(d2,d3,d1,12,12,15,15,0.5);\r\n\r\n" +
+                "droplet d4;\r\n" +
+                "droplet d5;\r\n" +
+                "split(d4,d5,d2,12,12,15,15,0.5);\r\n\r\n" +
+                "output(d3,0,0);\r\n" +
+                "output(d4,0,0);\r\n" +
+                "output(d5,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+        [TestMethod]
+        public void TestStorer()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "input(d1,1,1,1.0);\r\n" +
+                "store(d1,5,5,2.0);\r\n" +
+                "output(d1,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
+
+        [TestMethod]
+        public void TestMultiStorer()
+        {
+            string origin =
+                "droplet d1;\r\n" +
+                "input(d1,1,1,1.0);\r\n" +
+                "store(d1,5,5,2.0);\r\n" +
+                "store(d1,5,5,2.0);\r\n" +
+                "output(d1,0,0);\r\n";
+
+            Chip c = GetChipAndRun(origin, 20, 20);
+            Assert.AreEqual(true, c.manager.AllTasksCompleted());
+        }
 
         [TestMethod]
         public void TestAll()
@@ -81,11 +225,9 @@ namespace TestExecutor
                 "droplet d1;\r\n" +
                 "droplet d2;\r\n" +
                 "droplet d3;\r\n\r\n" +
-                "# droplet input\r\n" +
                 "input(d1,1,1,1.0);\r\n" +
                 "input(d2,4,4,0.5);\r\n" +
                 "input(d3,10,10,3.2);\r\n\r\n" +
-                "# move\r\n" +
                 "move(d1,3,3);\r\n" +
                 "move(d2,7,7);\r\n" +
                 "move(d3,9,9);\r\n\r\n" +
