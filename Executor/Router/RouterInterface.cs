3a64355355d51fa0b704d9ebfc30ed352d97f71f
Wenjie <wenjiedtu@gmail.com>
2022-12-02 01:48:23 +0100

    add option for row and column, router type;

--- Executor/Router/RouterInterface.cs
+++ Executor/Router/RouterInterface.cs
@@ -5,6 +5,5 @@ namespace Executor.Router
     public interface RouterInterface
     {
         public void MoveOneStep(Droplet d, int destx, int desty, List<Droplet> activeDrplets, List<Droplet> busyDroplets);
-
     }
 }
