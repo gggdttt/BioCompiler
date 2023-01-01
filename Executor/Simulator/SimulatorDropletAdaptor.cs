db28b879132abb3211afce0790391ca6bf71b0d3
Wenjie <wenjiedtu@gmail.com>
2023-01-02 00:52:36 +0100

    add comments

--- Executor/Simulator/SimulatorDropletAdaptor.cs
+++ Executor/Simulator/SimulatorDropletAdaptor.cs
@@ -9,11 +9,21 @@ namespace Executor.Simulator
 {
     public class SimulatorDropletAdaptor
     {
-        static int i = 0;
+        /// <summary>
+        /// This is used to record how many droplets there will be.
+        /// </summary>
+        static int DropletId = 0;
+
+        /// <summary>
+        /// Generate the droplets information for simulator's json file
+        /// </summary>
+        /// <param name="droplet">The droplets occur in this running</param>
+        /// <param name="width"> The width of the board(only need width to calculate the index of cell)</param>
+        /// <returns> A string in json format </returns>
         public static string GetDropletJsonFormatSimulatorInfo(Droplet droplet, int width)
         {
             return $" \"name\": \"{droplet.name}\",\r\n" +
-                $"\"ID\": {i++},\r\n" +
+                $"\"ID\": {DropletId++},\r\n" +
                 "\"substance_name\": \"h20\",\r\n" +
                 $"\"positionX\": {110 + droplet.xValue * 20 + droplet.gridDiameter * 20/2},\r\n" +
                 $"\"positionY\": {droplet.yValue * 20 + droplet.gridDiameter * 20 / 2},\r\n" +
--- Executor/Simulator/SimulatorFileGenerator.cs
+++ Executor/Simulator/SimulatorFileGenerator.cs
@@ -9,16 +9,24 @@ namespace Executor.Simulator
     public class SimulatorFileGenerator
     {
         /// <summary>
-        /// 
+        /// Generate a Basm file according to a template.
         /// </summary>
-        /// <param name="path"></param>
-        /// <param name="routerResult"></param>
+        /// <param name="outPutpath">The path to Basm File</param>
+        /// <param name="templatePath">The path of the Basm file template, used to replace content </param>
+        /// <param name="routerResult">A string containing the path of route</param>
         public static void SimulatorBasmFileGenerator(string outPutpath, string templatePath, string routerResult)
         {
             string text = File.ReadAllText(templatePath);
             File.WriteAllText(outPutpath, text.Replace(".text", ".text\r\nmain:\r\n" + routerResult + "\r\n"));
         }
 
+        /// <summary>
+        /// Generate the simulator .json file. It contains all the config of the board and the initilization of droplets
+        /// </summary>
+        /// <param name="outPutpath">The output path of file</param>
+        /// <param name="templatePath"> The path to the json template </param>
+        /// <param name="droplets">The droplets will show</param>
+        /// <param name="width"></param>
         public static void SimulatorJsonFileGenerator(string outPutpath, string templatePath, HashSet<Droplet> droplets, int width)
         {
             string text = File.ReadAllText(templatePath);
@@ -26,7 +34,12 @@ namespace Executor.Simulator
             File.WriteAllText(outPutpath, text);
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="droplets"></param>
+        /// <param name="width"></param>
+        /// <returns></returns>
         private static string GetAllDropletInitInfo(HashSet<Droplet> droplets, int width)
         {
             string result = string.Empty;
